8690efc876500c8b7f50164ca92a4962578b2cc2
Legaljay <adesinaayobami67@gmail.com>
2026-09-22 23:39:09 +0100

    fix: render deployment error

--- src/modules/cart/cart.module.ts
+++ src/modules/cart/cart.module.ts
@@ -1,14 +1,16 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Cart, CartSchema } from './schemas/cart.schema';
-import { CatalogModule } from '../catalog/catalog.module';
-import { CartService } from './cart.service';
-import { CartController } from './cart.controller';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Cart, CartSchema } from "./schemas/cart.schema";
+import { CatalogModule } from "../catalog/catalog.module";
+import { PromotionsModule } from "../promotions/promotions.module";
+import { CartService } from "./cart.service";
+import { CartController } from "./cart.controller";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Cart.name, schema: CartSchema }]),
     CatalogModule,
+    PromotionsModule,
   ],
   providers: [CartService],
   controllers: [CartController],
